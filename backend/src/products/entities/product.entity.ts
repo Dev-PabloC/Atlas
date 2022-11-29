a80462069147acb0cb2bcf32e9acde1f4d61c382
Carvalho <dstfunny@gmail.com>
2022-11-29 18:54:34 +0000

    Product fields

--- backend/src/products/entities/product.entity.ts
+++ backend/src/products/entities/product.entity.ts
@@ -1,7 +1,19 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { ObjectType, Field, Int, InputType } from "@nestjs/graphql";
 
 @ObjectType()
 export class Product {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @Field({ description: "ID category" })
+  categoryID: string;
+
+  @Field({ description: "Name of product" })
+  name: string;
+
+  @Field({ description: "Value product" })
+  value: number;
+
+  @Field({ description: "image name" })
+  imagePath: string;
+
+  @Field({ description: "Ingredients in product" })
+  ingredients: string[] | [] | string;
 }
