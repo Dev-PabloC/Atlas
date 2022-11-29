d16dbfcf03ac53e4e4150d061a80a73d9d785bc6
Carvalho <dstfunny@gmail.com>
2022-11-29 18:29:54 +0000

    creating auth folder for Validation user

---
+++ backend/src/auth/auth.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@nestjs/common';
+import { CreateAuthInput } from './dto/create-auth.input';
+import { UpdateAuthInput } from './dto/update-auth.input';
+
+@Injectable()
+export class AuthService {
+  
+}
